50edba82952180b23872a25a269df013f334231b
Marius <mariusherring@gmail.com>
2021-08-24 08:38:02 +0200

    Added some docs

---
+++ src/convts/loss/__tests__/loss.test.ts
@@ -0,0 +1,10 @@
+import React from "react";
+
+describe("Loss", () => {
+  test("MSE", () => {
+
+    expect(true).toEqual(true);
+  });
+});
+
+// https://mccormickml.com/2014/03/04/gradient-descent-derivation/
