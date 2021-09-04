5dd7f80e0d3faaaea492f783eeb6729d10c087db
Marius Herring <mariusherring@gmail.com>
2021-09-04 21:57:18 +0200

    Some padding

--- src/components/network/DisplayNetwork.tsx
+++ src/components/network/DisplayNetwork.tsx
@@ -7,7 +7,7 @@ const DisplayLayer = ({ layer }: { layer: Layer }) => {
     <div>
       <div
         className={
-          "p-4 m-4 max-w-sm mx-auto bg-gray-700 rounded-xl shadow-md flex items-center space-x-4"
+          "px-4 p-2 m-4 max-w-sm mx-auto bg-gray-700 rounded-xl shadow-md flex items-center space-x-4"
         }
       >
         {layer.name()}{" "}
